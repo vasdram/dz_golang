d4054311217185054fc6c20d71056a15f09f866b
vasiliykozlov <v.kozlov@dats.team>
2026-05-17 23:34:15 +0300

    fix review

--- less_1/main.go
+++ less_1/main.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	const USD_TO_EUR = 1.13
 	const USD_TO_RUB = 63.94
-	EUR_TO_RUB := USD_TO_RUB / USD_TO_EUR
+	const EUR_TO_RUB = USD_TO_RUB / USD_TO_EUR
 
 	fmt.Println(EUR_TO_RUB)
 }
